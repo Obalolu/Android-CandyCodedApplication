2b26cc576b782301a974914323b4e23aed73ed95
Obalolu <obaokemoney@gmail.com>
2019-11-09 22:28:51 +0100

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,7 +43,7 @@ public class InfoActivity extends AppCompatActivity {
     public void createPhoneIntent(View view) {
         Uri uri = Uri.parse("tel:0123456789");
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-        mapIntent.setData(uri);
-            startActivity(phoneIntent);
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
     }
 }
